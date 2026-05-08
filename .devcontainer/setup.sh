e75f3965a00e817bbcec4a2cbf527ff6c776aaee
Keith Roberts <ed_duran@hotmail.com>
2026-05-08 00:36:02 +0000

    Remove cached pyc files, update gitignore

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create config structure
+mkdir -p /config/custom_components
+mkdir -p /config/.storage
+
+# Symlink integration
+ln -sf /workspaces/ha_screenlogic_esp /config/custom_components/ha_screenlogic_esp
+
+# Copy config files
+cp /workspaces/ha_screenlogic_esp/.devcontainer/config/configuration.yaml /config/configuration.yaml
+
+# Restore dashboard
+cp /workspaces/ha_screenlogic_esp/.devcontainer/config/lovelace /config/.storage/lovelace
+
+pip install homeassistant
+
+### Enable stopping startup until debugger is started
+#export HA_DEBUG=true
+
